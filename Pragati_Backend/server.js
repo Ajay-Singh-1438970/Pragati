85ca262f929c48cf5d23662dd33c42b92d848367
Ajay Singh <ajaysingh1438970@gmail.com>
2025-10-30 03:07:12 +0530

    server.js temporary change

--- Pragati_Backend/server.js
+++ Pragati_Backend/server.js
@@ -1,53 +1,41 @@
-// import express from 'express';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-// import path, { dirname } from 'path';
-// import { fileURLToPath } from 'url';
-// import connectToDB from './config/db.js';
-// import authRoutes from './routes/authRoute.js';
-// import materialRoutes from './routes/materialRoute.js';
-// import userRoutes from './routes/userRoutes.js';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import connectToDB from './config/db.js';
+import authRoutes from './routes/authRoute.js';
+import materialRoutes from './routes/materialRoute.js';
+import userRoutes from './routes/userRoutes.js';
 
 
-// dotenv.config();
+dotenv.config();
 
-// const app = express();
-// const __filename = fileURLToPath(import.meta.url);
-// // const __dirname = dirname(__filename);
-// const __dirname = path.resolve();
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+const __dirname = path.resolve();
 
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// // Test route
-// app.get('/test', (req, res) => res.send("hello"));
+// Test route
+app.get('/test', (req, res) => res.send("hello"));
 
-// // API routes
-// app.use('/api/auth', authRoutes);
-// app.use('/api/materials', materialRoutes);
-// app.use("/api/users", userRoutes);
+// API routes
+app.use('/api/auth', authRoutes);
+app.use('/api/materials', materialRoutes);
+app.use("/api/users", userRoutes);
 
 
-// // Start server after DB connection
-// const port = process.env.PORT || 5000;
+// Start server after DB connection
+const port = process.env.PORT || 5000;
 
 // connectToDB()
 
-// app.listen(port, () => {
-//       console.log(`✅ Server running on ${port}`);
-//     });
-
-
-
-import express from 'express';
-const app = express();
-
-app.get('/test', (req, res) => {
-  res.send('Hello Render!');
-});
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+      console.log(`✅ Server running on ${port}`);
+    });
